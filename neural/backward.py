f4ffbbaad580b48f63a107ab7388b984b5f21205
zifeiyu0531 <32443516+zifeiyu0531@users.noreply.github.com>
2020-07-13 17:24:03 +0800

    反向传播

--- neural/backward.py
+++ neural/backward.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
-from neural import forward
+import forward
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
